be3a9f242517630f673d27287ad28159c0ce8a6b
Mark Cotner <mark.cotner@gmail.com>
2026-04-06 23:52:43 -0400

    Revert "Release v1.4.9"
    This reverts commit ccc2d803d3d90afea2aa2ff100c092b352a2f010.

--- lib/timeless_logs/index.ex
+++ lib/timeless_logs/index.ex
@@ -907,7 +907,7 @@ defmodule TimelessLogs.Index do
             end
           end,
           max_concurrency: batch_size,
-          ordered: true
+          ordered: false
         )
         |> Enum.flat_map(fn {:ok, entries} -> entries end)
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TimelessLogs.MixProject do
   def project do
     [
       app: :timeless_logs,
-      version: "1.4.9",
+      version: "1.4.8",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
